3012e56d6e34417bdc5b526028c7dc1b5b4a182e
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-11-20 15:35:10 -0600

    metodologiaEstrella

--- metodologiaEstrella.py
+++ metodologiaEstrella.py
@@ -3,6 +3,75 @@ import pandas as pd
 from itertools import combinations
 import random
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, classification_report
+from sympy.logic.boolalg import Or, And
+from sympy.logic.boolalg import simplify_logic
+from sympy.abc import symbols as sym_symbols
+
+def calcularMatrizConfusion(y_true, y_pred, targetClass):
+    verdaderos_positivos = 0
+    verdaderos_negativos = 0
+    falsos_positivos = 0
+    falsos_negativos = 0
+
+    for i in range(len(y_true)):
+        if y_true[i] == targetClass:
+            if y_pred[i] == targetClass:
+                verdaderos_positivos += 1
+            else:
+                falsos_negativos += 1
+        else:
+            if y_pred[i] != targetClass:
+                verdaderos_negativos += 1
+            else:
+                falsos_positivos += 1
+
+    accuracy = (verdaderos_positivos + verdaderos_negativos) / len(y_true)
+    recall = verdaderos_positivos / (verdaderos_positivos + falsos_negativos + 0.000001)
+    precision = verdaderos_positivos / (verdaderos_positivos + falsos_positivos + 0.0001)
+    f1_score = 2 * (precision * recall) / (precision + recall + 0.000001)
+
+    print("\nMatriz de Confusión:")
+    print("Verdaderos Positivos:", verdaderos_positivos)
+    print("Verdaderos Negativos:", verdaderos_negativos)
+    print("Falsos Positivos:", falsos_positivos)
+    print("Falsos Negativos:", falsos_negativos)
+    print("\nAccuracy:", accuracy)
+    print("Sensibilidad (Recall):", recall)
+    print("Precisión (Precision):", precision)
+    print("Puntuación F1 (F1-score):", f1_score)
+
+def expresionToLista(expression):
+    terms = expression.split('|') 
+    result = []
+
+    for term in terms:
+        conditions = term.strip().split('&') 
+        result.append([cond.strip() for cond in conditions])
+
+    return result
+
+def simplificarExpresionBooleana(expression):
+    disjunctions = []
+    symbols = set()
+
+    for conjunction in expression:
+        terms = []
+        for var in conjunction:
+            symbol = sym_symbols(var)
+            symbols.add(symbol)
+            terms.append(symbol)
+        disjunctions.append(And(*terms))
+
+    result = Or(*disjunctions)
+    simplified_result = simplify_logic(result)
+    
+    simplified_list = str(simplified_result).split('|')
+    final_list = []
+    for term in simplified_list:
+        conditions = term.strip().split('&')
+        final_list.append([cond.strip().replace('(', '').replace(')', '') for cond in conditions])
+
+    return simplified_result, symbols, final_list
 
 def evaluar(df_evaluacion, resultadosFinales, targetClass, elseClass, y_true):
     y_pred = []
@@ -22,15 +91,11 @@ def evaluar(df_evaluacion, resultadosFinales, targetClass, elseClass, y_true):
                 if all(elemento in fila for elemento in interUno[:-1]):
                     huboInterseccion = True
             if huboInterseccion:
-                print(f"La fila {fila}, fue evaluada como intersección")
+                print(f"La fila {fila[:-1]}, fue evaluada como intersección")
                 y_pred.append(targetClass)
                 break
    
-    matrizConfusion = confusion_matrix(y_true, y_pred)
-    print(f'\nMatriz de Confusión:\n{matrizConfusion}')
-
-    reporteClasificacion = classification_report(y_true, y_pred)
-    print(f'Reporte de Clasificación:\n{reporteClasificacion}')
+    calcularMatrizConfusion(y_true, y_pred, targetClass)
 
 def mezclar(df: pd.DataFrame):
     num_filas = len(df)
@@ -78,15 +143,19 @@ def aprendizaje(df_pos: pd.DataFrame, df_neg: pd.DataFrame, indice: int):
     
     expresionesConsistentes = []
     for combinacion in combinaciones_posibles:
-        for i, ejemplo in enumerate(ejemplosNegativos):
-            
+        i = 0
+        while(ejemplosNegativos and i != len(ejemplosNegativos)):
+            ejemplo = ejemplosNegativos[i]
             if all(elemento in ejemplo for elemento in combinacion):
                 # print("Incosistente->",combinacion)
+                ejemplosNegativos.remove(ejemplo)
                 break
                 
             if i == len(ejemplosNegativos) - 1:
                 # print("Cosistente->",combinacion)
                 expresionesConsistentes.append(combinacion)
+                
+            i += 1
                 
     
     # print("\nExpresiones consistentes:")
@@ -179,9 +248,14 @@ def algoritmoSTAR(df: pd.DataFrame, targetColumn):
         
     resultadosFinales = list(set(tuple(array) for array in complejoResultante))
         
-    resultado = f'{' V '.join(str(regla) for regla in resultadosFinales)} -> {targetClass}'
+    resultado = f'{" V ".join(str(regla) for regla in resultadosFinales)} -> {targetClass}'
     
     print("\nHipotesis aprendida: ", resultado)
+    
+    simplified_result, symbols, final_list = simplificarExpresionBooleana(resultadosFinales)
+    # print(final_list)
+    print("Expresión simplificada:", f'{" V ".join(str(tuple(regla)) for regla in final_list)} -> {targetClass}')
+    # print("Símbolos utilizados:", symbols)
     
     df_test_unido = pd.concat([df_pos_test, df_neg_test])
     df_evaluacion = []
@@ -195,7 +269,11 @@ def algoritmoSTAR(df: pd.DataFrame, targetColumn):
     
     y_true = list(df_test_unido.iloc[:, -1])
     
+    print("\n Evaluacion con hipotesis orginal")
     evaluar(df_evaluacion, resultadosFinales, targetClass, elseClass, y_true)
+    
+    print("\n Evaluacion con hipotesis simplificada")
+    evaluar(df_evaluacion, final_list, targetClass, elseClass, y_true)
     
     if intersecciones[0][0][-1] == 'Auto-pilot=YES':
         cad = 'YES'
